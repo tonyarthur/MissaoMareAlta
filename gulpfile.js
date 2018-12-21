d0009c5b16ec6fe86da17a96aa0be307659d431f
Your Name <tony_arthur@live.com>
2018-12-21 00:19:24 -0200

    Definição do header

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,39 @@
 var gulp = require('gulp');
+var watch = require('gulp-watch');
 var concat = require('gulp-concat');
 var cssmin = require('gulp-cssmin');
 var uncss = require('gulp-uncss');
+var browserSync = require('browser-sync').create();
+var reload      = browserSync.reload;
 
+gulp.task('serve', function () {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch('./style/*.css', gulp.series('css'));
+    gulp.watch('./js/*.js', gulp.series('js'));
+    gulp.watch("*.html").on("change", reload);
+});
+/*
+gulp.task('browser-sync', function(){
+    browserSync.init({
+        proxy: 'localhost:5000'
+    });
+
+    gulp.watch('./style/*.css', gulp.series('css'));
+    gulp.watch('./js/*.js', gulp.series('js'));
+});
+*/
 gulp.task('js', function(){
 
     return gulp.src(['./node_modules/bootstrap/dist/js/bootstrap.min.js',
                      './node_modules/jquery/dist/jquery.min.js',
+                     './js/script.js',
     ])
-    .pipe(gulp.dest('./assets/js/'));
+    .pipe(gulp.dest('./assets/js/'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('css', function(){
@@ -18,6 +43,8 @@ gulp.task('css', function(){
                      './node_modules/bootstrap/dist/css/bootstrap.css',
     ])
     .pipe(concat('marealta.min.css'))
-    .pipe(cssmin({html: ['teste.html ']}))
-    .pipe(gulp.dest('./assets/css'));
+    .pipe(cssmin())
+    .pipe(uncss({html: ['index.html ']}))
+    .pipe(gulp.dest('./assets/css'))
+    .pipe(browserSync.stream());
 });
